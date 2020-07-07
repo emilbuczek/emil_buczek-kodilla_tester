de5d86be5f2c0edfe5a449569dbc0d738ef36b7f
Emil Buczek <morfeox3@gmail.com>
2020-07-07 23:18:41 +0200

    new commit

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public class Main {
+        public static void main(String[] args) {
+            String myName = "Emil";
+            int myAge = 30;
+            double height = 1.95;
+            char carClass = 's';
+            boolean isGoodProgrammer = true;
+            System.out.println(myName);
+            System.out.println(myAge);
+            System.out.println(height);
+            System.out.println(carClass);
+
+
+        }
+    }
+}
