5fcce8dec6e49637cfac6619be8fc5a971fe8af9
Emil Buczek <morfeox3@gmail.com>
2020-10-08 14:53:33 +0200

    changes 1.4

--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/abstracts/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/abstracts/Application.java
@@ -1,7 +1,24 @@
 package com.kodilla.inheritance.abstracts;
 
-public class Application  {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
     public static void main(String[] args) {
-      System.out.println();
+        Person emil = new Person("Emil", 30, new Hunter("hunting", 3000));
+        Person karol = new Person("Karol", 25, new Baker("baking", 2000));
+        Person david = new Person("David", 40, new Programmer("programming", 40000));
+        Person michal = new Person("Michal", 30, new Soldier("shooting" , 4000));
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(emil);
+        personList.add(karol);
+        personList.add(david);
+        personList.add(michal);
+
+        for (Person person : personList){
+            System.out.println("Emil is a " + person.getJob() + " and he's" + " " + person.job.getNameOfResponsibilities() +
+                    " " + " and he's amount of salary is" + person.job.getSalary());
+        }
     }
 }
--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,8 +1,7 @@
 public class Application {
     public static void main(String[] args) {
-        Age age = new Age("adam", 40.5, 178);
-        System.out.println(age.name + age.age + age.height);
-        age.checkAge();
-
+        User user = new User("adam", 40.5, 178);
+        System.out.println(user.name + user.age + user.height);
+        user.checkAge();
     }
 }
