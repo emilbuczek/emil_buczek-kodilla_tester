5016e9400c567d3d1024017a5087d59f011af133
Emil Buczek <morfeox3@gmail.com>
2020-10-15 21:31:28 +0200

    6 kodilla-stream

---
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -0,0 +1,19 @@
+package com.kodilla.optional.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Janek", new Teacher("prof. Kowalski")));
+        students.add(new Student("Janek", new Teacher("prof. Nowak")));
+        students.add(new Student("Janek", null));
+        students.add(new Student("Janek", new Teacher("prof. Jagielski")));
+        students.add(new Student("Janek", null));
+        students.add(new Student("Janek", new Teacher("prof. Adamczyk")));
+
+        for (Student student : students)
+            System.out.println("Uczeń: " + student.getName() + ", nauczyciel: " + student.getTeacher().getName());
+    }
+}
